1e44f35f9d743aa3654d3658d5ec0447d76090a0
Victor Wibisono <victor@eatigo.com>
2018-10-13 13:40:06 +0800

    Modified map to foreach because of return type.

--- src/main/scala/net/martinprobson/spark/SparkMain.scala
+++ src/main/scala/net/martinprobson/spark/SparkMain.scala
@@ -2,5 +2,5 @@ package net.martinprobson.spark
 
 object SparkMain extends App {
   val spark = new SparkEnvClass
-  spark.versionInfo.map(v => println(v))
+  spark.versionInfo.foreach(v => println(v))
 }
